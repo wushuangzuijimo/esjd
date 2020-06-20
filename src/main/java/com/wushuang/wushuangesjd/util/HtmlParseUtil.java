d8fc9fd0975c493f69b94ef78e5c501925845b4a
wushuang <wushuang@qq.com>
2020-06-20 20:11:37 +0800

    go

--- src/main/java/com/wushuang/wushuangesjd/util/HtmlParseUtil.java
+++ src/main/java/com/wushuang/wushuangesjd/util/HtmlParseUtil.java
@@ -1,5 +1,6 @@
 package com.wushuang.wushuangesjd.util;
 
+import org.apache.lucene.analysis.standard.UAX29URLEmailTokenizerImpl;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -20,10 +21,11 @@ public class HtmlParseUtil {
 
         String url = "https://search.jd.com/Search?keyword=java";
         //解析网页(jsoup返回document就是document对象)
-        Document document = Jsoup.parse(new URL(url), 30000);
+        Document document = Jsoup.connect(url).timeout(30000).userAgent("aoligei").validateTLSCertificates(false).get();
 
 
         Element element = document.getElementById("J_goodsList");
+
 
         System.out.println(element);
 
